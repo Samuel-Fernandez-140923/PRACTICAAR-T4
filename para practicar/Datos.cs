6e23ad4de053725d418423cb1733ea6ee95deeae
Samuel-Fernandez-140923 <samueldukipkoko@gmail.com>
2024-11-26 01:17:39 -0500

    Primera

---
+++ para practicar/Datos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace para_practicar
+{
+    public class Datos
+    {
+        
+            public string Nombre { get; set; }
+            public int Edad { get; set; }
+            public double Tamaño { get; set; }
+
+
+
+            public override string ToString()
+            {
+                return "Nombre: "+Nombre+ " Edad: "+Edad+ "Tamaño:" +Tamaño;
+            }
+        }
+    }
+
---
+++ para practicar/Program.cs
@@ -0,0 +1,61 @@
+﻿
+using para_practicar;
+using static para_practicar.Datos;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int respuesta = 1;
+        int r2;
+        Datos datos = new Datos();
+
+        do
+        {
+            Console.Write("Cuantos datos ingresar: ");
+            respuesta = int.Parse(Console.ReadLine());
+                        
+        } while (respuesta <= 0);
+        string[] Nombre = new string[respuesta];
+        int[] Edad = new int[respuesta];
+        double[] Tamaño = new double[respuesta];
+
+        int posicion = 0;
+
+
+
+        do
+        {
+            Console.WriteLine("");
+            Console.WriteLine("\n**MENU**");
+            Console.WriteLine("CREAR [1]: ");
+
+            Console.Write("\n¿Que opcion quieres ingresar?: ");
+            r2 = int.Parse(Console.ReadLine());
+
+            switch (r2)
+            {
+                case 1:
+                    for (int i = 0; i < respuesta; i++)
+                    {
+                        Console.WriteLine("\n*********DATOS**   *******");
+                        Console.Write("INGRESA NOMBRE: ");
+                        datos.Nombre = Console.ReadLine();
+                        Nombre[posicion] = datos.Nombre;                
+                        Console.WriteLine("IGRESA LA EDAD:");
+                        datos.Edad = int.Parse(Console.ReadLine());
+                        Edad[posicion]=datos.Edad;
+                        Console.WriteLine("IGRESA EL TAMAÑO:");
+                        datos.Tamaño = double.Parse(Console.ReadLine());
+                        Tamaño[posicion] = datos.Tamaño;
+
+                        posicion++;
+                    }
+                    break;
+
+            }
+
+        } while (r2 != 2);
+    }
+}
+
